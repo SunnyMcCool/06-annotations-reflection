200c81c011d74f5942c86d5eaa421e5b1b77c3ea
SunnyMcCool <muellersa71566@th-nuernberg.de>
2021-04-26 21:43:26 +0200

    Musterloesung

--- src/main/java/ohm/softa/a06/App.java
+++ src/main/java/ohm/softa/a06/App.java
@@ -10,39 +10,39 @@ import ohm.softa.a06.model.Joke;
  * Created on 11/10/17.
  */
 
-class Employee {
-	private Integer id;
-	private String firstName;
-	private String lastName;
-
-	@SerializedName(value = "emailId", alternate = "emailAddress")
-	private String email;
-
-	Employee(Integer id, String f, String l, String m)
-	{
-		this.id = id;
-		this.firstName=f;
-		this.lastName=l;
-		this.email=m;
-	}
-}
+//class Employee {
+//	private Integer id;
+//	private String firstName;
+//	private String lastName;
+//
+//	@SerializedName(value = "emailId", alternate = "emailAddress")
+//	private String email;
+//
+//	Employee(Integer id, String f, String l, String m)
+//	{
+//		this.id = id;
+//		this.firstName=f;
+//		this.lastName=l;
+//		this.email=m;
+//	}
+//}
 
 public class App {
 
 	public static void main(String[] args) {
 		// TODO fetch a random joke and print it to STDOUT
-		Employee emp = new Employee(1001, "Lokesh", "Gupta", "howtodoinjava@gmail.com");
-
-		Gson gson1 = new GsonBuilder().setPrettyPrinting().create();
-
-		System.out.println(gson1.toJson(emp));
-
-		Gson gson = new Gson();
-		// JSON String --> Object
-		Joke j = gson.fromJson("{\"id\": 0, \"joke\": \"Haha.\"}", Joke.class);
-		// categories remains `null`
-		// Objec --> JSON String
-		String json = gson.toJson(j);
+//		Employee emp = new Employee(1001, "Lokesh", "Gupta", "howtodoinjava@gmail.com");
+//
+//		Gson gson1 = new GsonBuilder().setPrettyPrinting().create();
+//
+//		System.out.println(gson1.toJson(emp));
+//
+//		Gson gson = new Gson();
+//		// JSON String --> Object
+//		Joke j = gson.fromJson("{\"id\": 0, \"joke\": \"Haha.\"}", Joke.class);
+//		// categories remains `null`
+//		// Objec --> JSON String
+//		String json = gson.toJson(j);
 
 
 	}
--- src/main/java/ohm/softa/a06/ICNDBApi.java
+++ src/main/java/ohm/softa/a06/ICNDBApi.java
@@ -1,9 +1,35 @@
 package ohm.softa.a06;
 
-/**
- * @author Peter Kurfer
- * Created on 11/10/17.
- */
+import ohm.softa.a06.model.Joke;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+import java.util.List;
+
 public interface ICNDBApi {
 
+	@GET("/jokes/random")
+	Call<Joke> getRandomJoke();
+
+	@GET("/jokes/random")
+	Call<Joke> getRandomJoke(@Query("firstName") String firstName, @Query("lastName") String lastName);
+
+	@GET("/jokes/random/{count}")
+	Call<Joke[]> getRandomJokes(@Path("count") int count);
+
+	@GET("/jokes/random/{count}")
+	Call<Joke[]> getRandomJokes(@Path("count") int count, @Query("firstName") String firstName, @Query("lastName") String lastName);
+
+	@GET("/jokes/random/{count}")
+	Call<Joke[]> getRandomJokes(@Path("count") int count, @Query("limitTo") String[] categories);
+
+	@GET("/jokes/random/{count}")
+	Call<Joke[]> getRandomJokes(@Path("count") int count, @Query("limitTo") String[] categories, @Query("firstName") String firstName, @Query("lastName") String lastName);
+
+	@GET("/jokes/{id}")
+	Call<Joke> getJoke(@Path("id") int number);
 }
+
+
